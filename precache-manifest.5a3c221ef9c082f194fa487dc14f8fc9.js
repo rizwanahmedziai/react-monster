3c43e8020c213fd4dcf782577df3fe307b27d520
Muhammad Farhan Abrar <muhammadfarhan199@gmail.com>
2020-01-25 23:40:09 +0500

    Updates

---
+++ precache-manifest.5a3c221ef9c082f194fa487dc14f8fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f405c67d347a96d0cba6910d95c399a",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "62a6b83665f6da928fc8",
+    "url": "/react-monster/static/css/main.5fded9a2.chunk.css"
+  },
+  {
+    "revision": "364c6a7741c1458d55ea",
+    "url": "/react-monster/static/js/2.12430192.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster/static/js/2.12430192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62a6b83665f6da928fc8",
+    "url": "/react-monster/static/js/main.04a045ba.chunk.js"
+  },
+  {
+    "revision": "1859d5bc66c4d7de565e",
+    "url": "/react-monster/static/js/runtime-main.5bd882c2.js"
+  }
+]);
